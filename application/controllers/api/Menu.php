2b96eb6004e3a7c63ceef785281656872c185dea
hery_toavina <aherytoavina@gmail.com>
2021-01-20 16:51:38 +0300

    header

--- application/controllers/api/Menu.php
+++ application/controllers/api/Menu.php
@@ -1,4 +1,8 @@
 <?php   
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header('Access-Control-Allow-Headers: X-Requested-With');
+
     require APPPATH . '/libraries/REST_Controller.php';
     use Restserver\Libraries\REST_Controller;
    
